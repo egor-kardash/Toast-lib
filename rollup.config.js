ceb5ce8ea8ab85186efbecde2ee27a4b407ed7c6
Fritterus <mr.egorka.kardash.1999@gmail.com>
2022-10-24 09:54:11 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -18,6 +18,7 @@ export default [
     input: 'src/index.ts',
     output: [
       {
+        external: ['react', 'react-dom', 'styled-components'],
         file: pkg.main,
         format: 'cjs',
         sourcemap: true,
